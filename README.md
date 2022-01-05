0bb857f015f838e9f0b6dbe084a1f416c7a55a0d
Loïc STEINMETZ <loic.steinmetz2@gmail.com>
2022-01-05 22:00:22 +0100

    adds description

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Le contre mandat
+
+Le contre mandat est un projet développé avec le [Discord Insoumis](https://discord-insoumis.fr/).
+
+Le site permet de visualiser les contre-propositions réalisées par le groupe parlementaire de la France Insoumise durant le quinquennat Macron.
+
+On y trouvera donc compilés les 5 contre-budgets rédigés par le groupe parlementaire, qui permettent de faire état du mandat alternatif de la France Insoumise.
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,9 +12,20 @@ const Home: FC<Props> = ({onAction}) => {
             <div className={'home__logo'}><img className={'home__logo__image'} src={'/logo.png'} alt={'L\'avenir en commun'}/></div>
             <h1 className={'home__title'}>Le contre mandat</h1>
             <h2 className={'home__subtitle'}>2017-2022 : Le mandat alternatif de la France Insoumise</h2>
-            <Line />
+            <Line/>
             <div className={'home__action'}>
                 <button className={'home__action__button'} onClick={onAction}>Consulter le mandat alternatif</button>
+            </div>
+            <div className={'home__description'}>
+                <p className={'home__description__content'}><strong>Le contre mandat</strong> est un projet développé avec le <a
+                    href={'https://discord-insoumis.fr/'}>Discord
+                    Insoumis</a>.</p>
+                <p className={'home__description__content'}>Le site permet de visualiser les contre-propositions réalisées par le groupe parlementaire de la
+                    France Insoumise
+                    durant le quinquennat Macron.</p>
+                <p className={'home__description__content'}>On y trouvera donc compilés les 5 contre-budgets rédigés par le groupe parlementaire, qui permettent
+                    de faire
+                    état du mandat alternatif de la France Insoumise.</p>
             </div>
         </div>
     );
